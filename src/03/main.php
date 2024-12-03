cda83cdedfab5522122011f659c9f80aaa8975ec
Niels Gerritsen <n.gerritsen@protonmail.com>
2024-12-03 08:06:38 +0100

    Day 3

---
+++ src/03/main.php
@@ -0,0 +1,37 @@
+<?php
+
+$data = file_get_contents(__DIR__ . "/input.txt");
+
+function main(string $data)
+{
+  $instructions = parse_instructions($data);
+
+  echo calculate_total($instructions, false) . PHP_EOL;
+  echo calculate_total($instructions, true) . PHP_EOL;
+}
+
+function calculate_total(array $instructions, bool $use_skip): int
+{
+  $total = 0;
+  $skip = false;
+
+  foreach ($instructions as $instruction) {
+    if ($instruction == "do()") $skip = false;
+    if ($instruction == "don't()") $skip = true;
+    if ($instruction[0] !== "m") continue;
+    if ($skip && $use_skip) continue;
+    preg_match_all("/\d+/", $instruction, $nums);
+    $total += $nums[0][0] * $nums[0][1];
+  }
+
+  return $total;
+}
+
+function parse_instructions(string $data): array
+{
+  preg_match_all("/(mul\(\d+,\d+\))|(don\'t\(\))|(do\(\))/", $data, $matches);
+  return $matches[0];
+}
+
+
+main($data);
