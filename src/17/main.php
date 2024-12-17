d005675a1441d66f3a95774c6086cde10cc234a4
ngerritsen <n.gerritsen@proton.me>
2024-12-17 11:12:42 +0100

    Day 17

---
+++ src/17/main.php
@@ -0,0 +1,66 @@
+<?php
+
+$data = file_get_contents(__DIR__ . "/input.txt");
+
+function main(string $data)
+{
+    list($reg, $prog) = parse_computer($data);
+
+    echo run($reg, $prog) . PHP_EOL;
+    echo get_magic_a($reg, $prog) . PHP_EOL;
+}
+
+function get_magic_a(array $reg, array $prog): int
+{
+    $prog_str = implode(",", $prog);
+    $a = 0;
+
+    while (true) {
+        $ans = run(array_merge([$a], $reg), $prog);
+
+        if (!str_ends_with($prog_str, $ans)) {
+            $a++;
+            continue;
+        }
+
+        if ($prog_str === $ans) return $a;
+
+        $a = $a << 3;
+    }
+}
+
+function run(array $reg, array $prog): string
+{
+    $res = [];
+
+    for ($p = 0; $p < count($prog); $p += 2) {
+        $i = $prog[$p];
+        $v = $prog[$p + 1];
+
+        if ($i === 0 || $i >= 6) $reg[$i % 5] = $reg[0] >> get_combo($reg, $v);
+        if ($i === 1) $reg[1] = $reg[1] ^ $v;
+        if ($i === 2) $reg[1] = get_combo($reg, $v) % 8;
+        if ($i === 3 && $reg[0] !== 0) $p = $v - 2;
+        if ($i === 4) $reg[1] = $reg[1] ^ $reg[2];
+        if ($i === 5) $res[] = get_combo($reg, $v) % 8;
+    }
+
+    return implode(",", $res);
+}
+
+function get_combo(array $reg, int $val): int
+{
+    if ($val <= 3) return $val;
+    return $reg[$val - 4];
+}
+
+function parse_computer(string $data)
+{
+    list($t, $b) = explode("\n\n", trim($data));
+    $reg = array_map(fn($l) => (int)explode(": ", $l)[1], explode("\n", $t));
+    $p = explode(": ", $b)[1];
+    $prog = array_map("intval", explode(",", $p));
+    return [$reg, $prog];
+}
+
+main($data);
